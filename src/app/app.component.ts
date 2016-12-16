90ca071deb889307d084d9ddf6873b90ea5993d6
Stuart Finley <G517329@genmills.com>
2016-12-16 13:50:44 -0600

    bring in assets from bhmc1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `<div class="jumbotron"><h1>Hello {{name}}</h1></div>`,
 })
 export class AppComponent  { name = 'Angular'; }
